df647a22549d5095de9a2aff33bd0058ea9e2539
Shittu Taiwo <101399654+Moyosof@users.noreply.github.com>
2025-08-31 01:30:16 +0100

    fix deployment

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -o errexit # Exit on error 
 
-pip install -r requirements.txt # Install dependencies from requirements.txt file 
+pip install -r requirement.txt # Install dependencies from requirements.txt file 
 
 python manage.py collectstatic --noinput # Collect static files. 
 python manage.py migrate # Apply database migrations.
